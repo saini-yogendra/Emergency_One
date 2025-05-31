732d24c5d229ff5d91a19e0ff85c722b334255c7
saini-yogendra <sainiyogendra1701@gmail.com>
2025-06-01 00:42:17 +0530

    [Firebase] firebase setup for android and ios done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:emergencyone/core/themes/themes.dart';
 import 'package:emergencyone/features/auth/view/auth_view.dart.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
